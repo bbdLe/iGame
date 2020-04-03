2adb14feda2b66cfab9ec2b2b646319592bf9c16
bbdLe <dalibaxiaoliba19@gmail.com>
2020-04-03 14:19:09 +0800

    [feat]RawPacket

--- comm/session/session.go
+++ comm/session/session.go
@@ -1,6 +1,9 @@
 package session
 
-import "github.com/bbdLe/iGame/comm/peer"
+import (
+	"github.com/bbdLe/iGame/comm/meta"
+	"github.com/bbdLe/iGame/comm/peer"
+)
 
 type Session interface {
 	Raw() interface{}
@@ -12,4 +15,27 @@ type Session interface {
 	Close()
 
 	ID() int64
+}
+
+type RawPacket struct {
+	Data []byte
+	MsgID int
+}
+
+func (self *RawPacket) Message() interface{} {
+	if self.MsgID == 0 {
+		return struct{}{}
+	}
+
+	meta := meta.MessageMetaByID(self.MsgID)
+	if meta == nil {
+		return struct{}{}
+	}
+
+	obj := meta.NewType()
+	if err := meta.Codec.Decode(self.Data, obj); err != nil {
+		return struct{}{}
+	} else {
+		return obj
+	}
 }
