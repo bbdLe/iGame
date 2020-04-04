4986b8fa2f41c9a484108758c5e329d092dea3bc
bbdLe <dalibaxiaoliba19@gmail.com>
2020-04-04 22:38:08 +0800

    [feat]增加error类

---
+++ comm/err/err.go
@@ -0,0 +1,29 @@
+package err
+
+import "fmt"
+
+type Error struct {
+	s string
+	context interface{}
+}
+
+func (self *Error) Error() string {
+	if self.context == nil {
+		return self.s
+	} else {
+		return fmt.Sprintf("%s '%v'", self.s, self.context)
+	}
+}
+
+func NewError(s string) error {
+	return &Error{
+		s : s,
+	}
+}
+
+func NewErrorContext(s string, context interface{}) error {
+	return &Error{
+		s : s,
+		context : context,
+	}
+}
