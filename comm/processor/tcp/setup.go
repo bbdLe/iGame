c8171b1369901d2932228a55c017dd0aa839c346
bbdLe <dalibaxiaoliba19@gmail.com>
2020-04-07 00:04:33 +0800

    [fix]修复tcp proc注册错误

--- comm/processor/tcp/setup.go
+++ comm/processor/tcp/setup.go
@@ -3,7 +3,7 @@ package tcp
 import "github.com/bbdLe/iGame/comm/processor"
 
 func init() {
-	processor.RegProcessor("tcp.tlv", func(bundle processor.ProcessorBundle, cb processor.EventCallback, args ...interface{}) {
+	processor.RegProcessor("tcp.ltv", func(bundle processor.ProcessorBundle, cb processor.EventCallback, args ...interface{}) {
 		bundle.SetTransmitter(new(TCPMessageTransmitter))
 		bundle.SetHooker(new(MsgHooker))
 		bundle.SetCallback(cb)
