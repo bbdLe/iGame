13e4387759a234ccced19b517e874eb1f0a1f59c
bbdLe <dalibaxiaoliba19@gmail.com>
2020-04-07 21:09:09 +0800

    [feat]udp session

--- comm/peer/udp/session.go
+++ comm/peer/udp/session.go
@@ -1,3 +1,21 @@
 package udp
 
+import (
+	"github.com/bbdLe/iGame/comm"
+	"github.com/bbdLe/iGame/comm/peer"
+)
 
+type DataReader interface {
+	ReadData() []byte
+}
+
+type DataWriter interface {
+	WriteData(data []byte)
+}
+
+type udpSession struct {
+	*peer.CoreProcBundle
+	peer.CoreContextSet
+
+	pInterface comm.Peer
+}
